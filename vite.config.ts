2f1d4ee992823df9bc6e285fadf3baf8d5d62c0a
halkompleksi33-commits <halkompleksi33@gmail.com>
2026-09-20 15:26:56 +0300

    Configure production D1 database for contributor data

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { readExecutionProfile } from "./scripts/execution-profile.mjs";
 import { sites } from "./build/sites-vite-plugin";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+  "b25f863d-4e66-4109-8503-4c2764d927a6";
 
 const { d1, r2 } = hostingConfig;
 
